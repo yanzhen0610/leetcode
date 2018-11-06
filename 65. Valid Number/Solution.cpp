77d40ca0bf199e81136ec7cc82c35bcb391e7815
Yz <yanzhen610@gmail.com>
2018-11-07 02:48:11 +0800

    updated 65

--- 65. Valid Number/Solution.cpp
+++ 65. Valid Number/Solution.cpp
@@ -3,6 +3,7 @@ public:
     bool isNumber(string s) {
         const char *p = s.c_str(), *q;
         
+        // trim left
         while (std::isspace(*p)) ++p;
         if (*p == '+' || *p == '-') ++p;
         q = p;
@@ -10,6 +11,7 @@ public:
         while (std::isdigit(*p)) ++p;
         if (*p == '.')
         {
+            // no digit(s)
             if (p == q && !std::isdigit(*(p + 1))) return false;
             ++p;
             while (std::isdigit(*p)) ++p;
@@ -17,15 +19,19 @@ public:
         
         if (*p == 'e')
         {
+            // no number before e
             if (p++ == q) return false;
             if (*p == '+' || *p == '-') ++p;
+            // no digit after e
             if (!std::isdigit(*p)) return false;
             ++p;
             while (std::isdigit(*p)) ++p;
         }
         
+        // trim right
         while (std::isspace(*p)) ++p;
         
+        // not empty and read to the end
         return p != q && *p == 0;
     }
 };
