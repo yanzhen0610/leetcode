d65c70aad02d8bb62bb4ec3162d6ae07fb8067e3
Yz <yanzhen610@gmail.com>
2018-11-07 00:15:48 +0800

    updated 814

--- 814. Binary Tree Pruning/Solution.cpp
+++ 814. Binary Tree Pruning/Solution.cpp
@@ -10,10 +10,9 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        if (!root) return nullptr;
-        root->left = pruneTree(root->left);
-        root->right = pruneTree(root->right);
+        if (root->left) root->left = pruneTree(root->left);
+        if (root->right) root->right = pruneTree(root->right);
         if (root->val || root->left || root->right) return root;
-        else return nullptr;
+        return nullptr;
     }
 };
