a758926f1d6aae89d7cdad30bb53b5619baad674
yanzhen0610 <yanzhen610@gmail.com>
2018-11-10 00:08:26 +0800

    Update and rename speedup.cpp to disable_cpp_iostream_sync.cpp

---
+++ utils/disable_cpp_iostream_sync.cpp
@@ -0,0 +1,1 @@
+static const auto speedup = []() { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
--- utils/speedup.cpp
+++
@@ -1,1 +0,0 @@
-static const auto speedup = []() { std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0; }();
