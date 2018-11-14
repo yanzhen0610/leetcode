24d79c542530a65cf691bae3adfe453cb5ce0621
Yz <yanzhen610@gmail.com>
2018-11-14 15:12:54 +0800

    updated 937

--- 937. Reorder Log Files/Solution.cpp
+++ 937. Reorder Log Files/Solution.cpp
@@ -1,22 +1,17 @@
-struct {
-    bool operator()(string a, string b)
-    {
-        return strcmp(a.c_str() + a.find(' '), b.c_str() + b.find(' ')) < 0;
-    }
-} cmp;
-
-bool only_digit(const string& s)
-{
-    return isdigit(*(s.c_str() + s.find(' ') + 1));
-}
-
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
         vector<string> result;
         for (const auto& log : logs) if (!only_digit(log)) result.push_back(log);
-        sort(result.begin(), result.end(), cmp);
+        sort(result.begin(), result.end(), [](string a, string b) -> bool {
+            return strcmp(a.c_str() + a.find(' '), b.c_str() + b.find(' ')) < 0;
+        });
         for (const auto& log : logs) if (only_digit(log)) result.push_back(log);
         return result;
+    }
+private:
+    bool only_digit(const string& s)
+    {
+        return isdigit(*(s.c_str() + s.find(' ') + 1));
     }
 };
