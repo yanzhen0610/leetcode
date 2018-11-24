768b230aa01d776cd87b5f34f836bedfe0dd1930
Yz <yanzhen610@gmail.com>
2018-11-25 03:04:45 +0800

    added blank lines for readability

--- 106. Construct Binary Tree from Inorder and Postorder Traversal/solution.c
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal/solution.c
@@ -22,11 +22,14 @@ int* linear_search(int* a, int l, int t)
 }
 struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
     if (inorderSize <= 0 || postorderSize <= 0) return NULL;
+
     int root_value = postorder[postorderSize - 1];
     int* root_inorder = linear_search(inorder, inorderSize, root_value);
     int left_len = root_inorder - inorder;
     int right_len = inorderSize - left_len - 1;
+
     struct TreeNode* left_subtree = buildTree(inorder, left_len, postorder, left_len);
     struct TreeNode* right_subtree = buildTree(root_inorder + 1, right_len, postorder + left_len, right_len);
+    
     return new_node(root_value, left_subtree, right_subtree);
 }
