e825febca173f9ad6976fccc863e6df50e84e9fb
Yz <yanzhen610@gmail.com>
2018-12-02 12:06:11 +0800

    added 951

---
+++ 951. Flip Equivalent Binary Trees/solution.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2) {
+    if (!root1 && root2) return false;
+    if (root1 && !root2) return false;
+    if (root1 == root2) return true;
+    if (root1->val != root2->val) return false;
+    return (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left)) ||
+        (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right));
+}
