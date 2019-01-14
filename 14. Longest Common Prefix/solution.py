158af0d55fc8690d73b7afc68eeaa66751927190
Yz <yanzhen610@gmail.com>
2019-01-14 15:33:30 +0800

    added 14

---
+++ 14. Longest Common Prefix/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        strs.sort()
+        for i in range(len(strs[0])):
+            if strs[0][i] != strs[-1][i]:
+                return strs[0][:i]
+        return strs[0][:len(strs[0])]
